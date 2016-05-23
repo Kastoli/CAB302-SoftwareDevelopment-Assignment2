df4531dc7a0331528e8ef2d65d0e78fe1f88a71e
Kastoli <kastoli@gmail.com>
2016-05-23 20:33:32 +1000

    Fiddling around with Swing
    What i've added might help develop something in the future!

--- AircraftSimulator/src/asgn2Simulators/GUISimulator.java
+++ AircraftSimulator/src/asgn2Simulators/GUISimulator.java
@@ -9,13 +9,30 @@ package asgn2Simulators;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 
 /**
  * @author hogan
  *
  */
 @SuppressWarnings("serial")
-public class GUISimulator extends JFrame implements Runnable {
+public class GUISimulator extends JFrame implements Runnable {	
+	JPanel p = new JPanel();
+	JButton b = new JButton("Hello");
+	JTextField t = new JTextField("Hi", 20);
+	JTextArea ta = new JTextArea("How\nAre\nYou?", 5, 20);
+	JLabel l = new JLabel("What's up");
+	String choices[] = {
+		"Hallo",
+		"Bonjour",
+		"Guten Tag"
+	};
+	JComboBox cb = new JComboBox(choices);
 
 	/**
 	 * @param arg0
@@ -23,7 +40,19 @@ public class GUISimulator extends JFrame implements Runnable {
 	 */
 	public GUISimulator(String arg0) throws HeadlessException {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+		
+		setSize(400,300);
+		setResizable(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		p.add(b);
+		p.add(t);
+		p.add(ta);
+		p.add(l);
+		p.add(cb);
+		add(p);
+		
+		setVisible(true);
 	}
 
 	/* (non-Javadoc)
@@ -40,7 +69,7 @@ public class GUISimulator extends JFrame implements Runnable {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new GUISimulator("Window Title");
 	}
 
 }
