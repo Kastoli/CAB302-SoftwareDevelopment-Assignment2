7ec8b2cb60d9ce41af436f17777a4abc5a8293a5
Kastoli <kastoli@gmail.com>
2016-05-27 12:20:53 +1000

    Layout for GUI Log Complete
    I'm not sure which of these files are necessary not to lose data, so i'm
    including them all.

--- AircraftSimulator/src/asgn2Simulators/GUISimulator.java
+++ AircraftSimulator/src/asgn2Simulators/GUISimulator.java
@@ -6,16 +6,17 @@
  */
 package asgn2Simulators;
 
-import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.HeadlessException;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
 
 /**
  * @author hogan
@@ -61,6 +62,8 @@ public class GUISimulator extends JFrame implements Runnable {
 	JPanel p10 = new JPanel();
 	JPanel p11 = new JPanel();
 	JPanel p12 = new JPanel();
+	private final JPanel panel = new JPanel();
+	private final JPanel panel_1 = new JPanel();
 	
 	/**
 	 * @param arg0
@@ -69,54 +72,72 @@ public class GUISimulator extends JFrame implements Runnable {
 	public GUISimulator(String arg0) throws HeadlessException {
 		super(arg0);
 		
-		setSize(800,600);
+		setSize(800,540);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		p1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		getContentPane().add(p1);
+		
+		p1.add(panel);
+		panel.add(ta);
+		
+		p1.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 3, 80, 0));
+		l1.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		l1.setFont(new Font("Arial", Font.PLAIN, 30));
-		l2.setFont(new Font("Arial", Font.PLAIN, 30));
-		l3.setFont(new Font("Arial", Font.PLAIN, 30));
+		p5.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		p5.add(l4);
 		p5.add(t1);
-		p6.add(l5);
-		p6.add(t2);
+		p7.setLayout(new GridLayout(0, 2, 0, 0));
 		p7.add(l6);
 		p7.add(t3);
+		p8.setLayout(new GridLayout(0, 2, 0, 0));
 		p8.add(l7);
 		p8.add(t4);
+		panel_1.add(p2);
+		p2.setLayout(new GridLayout(5, 1, 0, 0));
 		
 		p2.add(l1);
 		p2.add(p5);
 		p2.add(p6);
+		p6.setLayout(new GridLayout(0, 2, 0, 0));
+		p6.add(l5);
+		p6.add(t2);
 		p2.add(p7);
 		p2.add(p8);
+		l2.setHorizontalAlignment(SwingConstants.CENTER);
+		l2.setFont(new Font("Arial", Font.PLAIN, 30));
+		p9.setLayout(new GridLayout(0, 2, 0, 0));
+		p9.add(l8);
 		
 		p9.add(t5);
-		p9.add(l8);
-		p10.add(t6);
+		p10.setLayout(new GridLayout(0, 2, 0, 0));
 		p10.add(l9);
-		p11.add(t7);
+		p10.add(t6);
+		p11.setLayout(new GridLayout(0, 2, 0, 0));
 		p11.add(l10);
-		p12.add(t8);
+		p11.add(t7);
+		p12.setLayout(new GridLayout(0, 2, 0, 0));
 		p12.add(l11);
+		p12.add(t8);
+		panel_1.add(p3);
+		p3.setLayout(new GridLayout(5, 1, 0, 0));
 		
 		p3.add(l2);
 		p3.add(p9);
 		p3.add(p10);
 		p3.add(p11);
 		p3.add(p12);
-		
+		l3.setHorizontalAlignment(SwingConstants.CENTER);
+		l3.setFont(new Font("Arial", Font.PLAIN, 30));
+		panel_1.add(p4);
+		p4.setLayout(new GridLayout(5, 1, 0, 0));
 		p4.add(l3);
 		p4.add(b1);
 		p4.add(b2);
-		
-		p1.add(ta);
-		p1.add(p2);
-		p1.add(p3);
-		p1.add(p4);
-		
-		add(p1);
 		
 		setVisible(true);
 	}
