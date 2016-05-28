80bf1109d6e94f66f5f204b4f688a1b2f2d703f7
Kastoli <kastoli@gmail.com>
2016-05-28 14:21:30 +1000

    Tidied Up GUI Code

--- AircraftSimulator/src/asgn2Simulators/GUISimulator.java
+++ AircraftSimulator/src/asgn2Simulators/GUISimulator.java
@@ -23,47 +23,52 @@ import javax.swing.SwingConstants;
  *
  */
 @SuppressWarnings("serial")
-public class GUISimulator extends JFrame implements Runnable {	
+public class GUISimulator extends JFrame implements Runnable {
+	// Create Text Area
 	JTextArea ta = new JTextArea("", 19, 69);
+	
+	// Create Labels
 	JLabel l1 = new JLabel("Simulation");
 	JLabel l2 = new JLabel("Face Classes");
 	JLabel l3 = new JLabel("Operation");
 	JLabel l4 = new JLabel("RNG Seed");
-	JTextField t1 = new JTextField(8);
 	JLabel l5 = new JLabel("Daily Mean");
-	JTextField t2 = new JTextField(8);
-	JLabel l6 = new JLabel("Queue Size");
-	JTextField t3 = new JTextField(8);
+	JLabel l6 = new JLabel("Queue Size");	
 	JLabel l7 = new JLabel("Cancellation");
-	JTextField t4 = new JTextField(8);
 	JLabel l8 = new JLabel("First");
-	JTextField t5 = new JTextField(8);
 	JLabel l9 = new JLabel("Business");
-	JTextField t6 = new JTextField(8);
 	JLabel l10 = new JLabel("Premium");
-	JTextField t7 = new JTextField(8);
 	JLabel l11 = new JLabel("Economy");
+	
+	// Create Text Fields
+	JTextField t1 = new JTextField(8);
+	JTextField t2 = new JTextField(8);
+	JTextField t3 = new JTextField(8);
+	JTextField t4 = new JTextField(8);
+	JTextField t5 = new JTextField(8);
+	JTextField t6 = new JTextField(8);
+	JTextField t7 = new JTextField(8);
 	JTextField t8 = new JTextField(8);
+	
+	// Create Buttons
 	JButton b1 = new JButton("Run Simulation");
 	JButton b2 = new JButton("Show Chart 2");
 	
+	// Create Panels
 	JPanel p1 = new JPanel();
-	
 	JPanel p2 = new JPanel();
 	JPanel p3 = new JPanel();
 	JPanel p4 = new JPanel();
-	
 	JPanel p5 = new JPanel();
 	JPanel p6 = new JPanel();
 	JPanel p7 = new JPanel();
 	JPanel p8 = new JPanel();
-	
 	JPanel p9 = new JPanel();
 	JPanel p10 = new JPanel();
 	JPanel p11 = new JPanel();
 	JPanel p12 = new JPanel();
-	private final JPanel panel = new JPanel();
-	private final JPanel panel_1 = new JPanel();
+	JPanel p13 = new JPanel();
+	JPanel p14 = new JPanel();
 	
 	/**
 	 * @param arg0
@@ -72,73 +77,71 @@ public class GUISimulator extends JFrame implements Runnable {
 	public GUISimulator(String arg0) throws HeadlessException {
 		super(arg0);
 		
+		// Setup Frame
 		setSize(800,540);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// Setup Panels
 		p1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		p1.add(p13);
+			p13.add(ta);
+		p1.add(p14);
+			p14.setLayout(new GridLayout(0, 3, 80, 0));
+			p14.add(p2);
+				p2.setLayout(new GridLayout(5, 1, 0, 0));
+				p2.add(l1);
+					l1.setHorizontalAlignment(SwingConstants.CENTER);
+					l1.setFont(new Font("Arial", Font.PLAIN, 30));
+				p2.add(p5);
+					p5.setLayout(new GridLayout(0, 2, 0, 0));
+					p5.add(l4);
+					p5.add(t1);
+				p2.add(p6);
+					p6.setLayout(new GridLayout(0, 2, 0, 0));
+					p6.add(l5);
+					p6.add(t2);
+				p2.add(p7);
+					p7.setLayout(new GridLayout(0, 2, 0, 0));
+					p7.add(l6);
+					p7.add(t3);
+				p2.add(p8);
+					p8.setLayout(new GridLayout(0, 2, 0, 0));
+					p8.add(l7);
+					p8.add(t4);
+			p14.add(p3);
+				p3.setLayout(new GridLayout(5, 1, 0, 0));
+				p3.add(l2);
+					l2.setHorizontalAlignment(SwingConstants.CENTER);
+					l2.setFont(new Font("Arial", Font.PLAIN, 30));
+				p3.add(p9);
+					p9.setLayout(new GridLayout(0, 2, 0, 0));
+					p9.add(l8);
+					p9.add(t5);
+				p3.add(p10);
+					p10.setLayout(new GridLayout(0, 2, 0, 0));
+					p10.add(l9);
+					p10.add(t6);
+				p3.add(p11);
+					p11.setLayout(new GridLayout(0, 2, 0, 0));
+					p11.add(l10);
+					p11.add(t7);
+				p3.add(p12);
+					p12.setLayout(new GridLayout(0, 2, 0, 0));
+					p12.add(l11);
+					p12.add(t8);
+			p14.add(p4);
+				p4.setLayout(new GridLayout(5, 1, 0, 0));
+				p4.add(l3);
+					l3.setHorizontalAlignment(SwingConstants.CENTER);
+					l3.setFont(new Font("Arial", Font.PLAIN, 30));
+				p4.add(b1);
+				p4.add(b2);
+
+		// Add Panels to Window
+		add(p1);
 		
-		getContentPane().add(p1);
-		
-		p1.add(panel);
-		panel.add(ta);
-		
-		p1.add(panel_1);
-		panel_1.setLayout(new GridLayout(0, 3, 80, 0));
-		l1.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		l1.setFont(new Font("Arial", Font.PLAIN, 30));
-		p5.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		p5.add(l4);
-		p5.add(t1);
-		p7.setLayout(new GridLayout(0, 2, 0, 0));
-		p7.add(l6);
-		p7.add(t3);
-		p8.setLayout(new GridLayout(0, 2, 0, 0));
-		p8.add(l7);
-		p8.add(t4);
-		panel_1.add(p2);
-		p2.setLayout(new GridLayout(5, 1, 0, 0));
-		
-		p2.add(l1);
-		p2.add(p5);
-		p2.add(p6);
-		p6.setLayout(new GridLayout(0, 2, 0, 0));
-		p6.add(l5);
-		p6.add(t2);
-		p2.add(p7);
-		p2.add(p8);
-		l2.setHorizontalAlignment(SwingConstants.CENTER);
-		l2.setFont(new Font("Arial", Font.PLAIN, 30));
-		p9.setLayout(new GridLayout(0, 2, 0, 0));
-		p9.add(l8);
-		
-		p9.add(t5);
-		p10.setLayout(new GridLayout(0, 2, 0, 0));
-		p10.add(l9);
-		p10.add(t6);
-		p11.setLayout(new GridLayout(0, 2, 0, 0));
-		p11.add(l10);
-		p11.add(t7);
-		p12.setLayout(new GridLayout(0, 2, 0, 0));
-		p12.add(l11);
-		p12.add(t8);
-		panel_1.add(p3);
-		p3.setLayout(new GridLayout(5, 1, 0, 0));
-		
-		p3.add(l2);
-		p3.add(p9);
-		p3.add(p10);
-		p3.add(p11);
-		p3.add(p12);
-		l3.setHorizontalAlignment(SwingConstants.CENTER);
-		l3.setFont(new Font("Arial", Font.PLAIN, 30));
-		panel_1.add(p4);
-		p4.setLayout(new GridLayout(5, 1, 0, 0));
-		p4.add(l3);
-		p4.add(b1);
-		p4.add(b2);
-		
+		// Make Window Visible
 		setVisible(true);
 	}
 
