3949a9874707e78c903f900f5f5f0381f0a98592
brochecouste <b.rochecouste@gmail.com>
2016-05-30 21:15:25 +1000

    tests
    tests

--- AircraftSimulator/src/asgn2Tests/AircraftTests.java
+++ AircraftSimulator/src/asgn2Tests/AircraftTests.java
@@ -24,11 +24,26 @@ public class AircraftTests {
 	@Test
 	public void confirmBookingTest() throws AircraftException, PassengerException {
 		Economy pE = new Economy(1000,1200);
+		Business pB = new Business(1000,1200);
 		A380 testFlight = new A380("SEA111", 1200);
 		testFlight.confirmBooking(pE, 1100);
+		testFlight.confirmBooking(pB, 1100);
 		assertEquals(1, testFlight.getNumEconomy());
-		assertEquals(1, testFlight.getNumPassengers());
+		assertEquals(2, testFlight.getNumPassengers());
 		assertTrue(testFlight.hasPassenger(pE));
+	}
+	
+	@Test
+	public void cancelBookingTest() throws AircraftException, PassengerException {
+		Economy pE = new Economy(1000,1200);
+		Business pB = new Business(1000,1200);
+		A380 testFlight = new A380("SEA111", 1200);
+		testFlight.confirmBooking(pE, 1100);
+		testFlight.confirmBooking(pB, 1100);
+		testFlight.cancelBooking(pB, 1130);
+		assertEquals(0, testFlight.getNumBusiness());
+		assertEquals(1, testFlight.getNumPassengers());
+		assertTrue(!testFlight.hasPassenger(pB));
 	}
 
 }
