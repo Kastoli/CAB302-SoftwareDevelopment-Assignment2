9023eb6ae8937039d4dadabddc34f9e06ccfb71a
Kastoli <kastoli@gmail.com>
2016-06-01 16:32:39 +1000

    Update

--- AircraftSimulator/src/asgn2Simulators/XYChartPanel.java
+++ AircraftSimulator/src/asgn2Simulators/XYChartPanel.java
@@ -1,8 +1,5 @@
 package asgn2Simulators;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -26,15 +23,9 @@ public class XYChartPanel extends ApplicationFrame {
 		ChartPanel chartPanel = new ChartPanel(XYLineChart);
 		chartPanel.setPreferredSize(new java.awt.Dimension(800, 540));
 		XYPlot plot = XYLineChart.getXYPlot();
-		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
-	    renderer.setSeriesPaint( 0 , Color.RED );
-	    renderer.setSeriesPaint( 1 , Color.GREEN );
-	    renderer.setSeriesPaint( 2 , Color.YELLOW );
-	    renderer.setSeriesStroke( 0 , new BasicStroke( 4.0f ) );
-	    renderer.setSeriesStroke( 1 , new BasicStroke( 3.0f ) );
-	    renderer.setSeriesStroke( 2 , new BasicStroke( 2.0f ) );
-	    plot.setRenderer( renderer ); 
-	    setContentPane( chartPanel );
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+	    plot.setRenderer(renderer); 
+	    setContentPane(chartPanel);
 	}
 	
 	public static void main(XYDataset dataset){
